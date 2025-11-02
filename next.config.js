ccc80767a445babbdc7074e28b4b810831929dfc
GUILLERMO RECALDE <recaldev.ga@gmail.com>
2025-11-02 20:28:04 -0300

    Agregado segundo formato de denuncia, experimental no estable

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-        ],
-      },
-    ]
+  generateBuildId: async () => {
+    return `${Date.now()}`
   },
 }
 
