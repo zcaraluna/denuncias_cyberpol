6a1e638e4550c9f9528fb9868e00192c594a9a31
GUILLERMO RECALDE <recaldev.ga@gmail.com>
2025-11-02 20:26:28 -0300

    Agregado segundo formato de denuncia, experimental no estable

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
