74dd1c543e023158a4f4dd154b8a86c61bb04b70
GUILLERMO RECALDE <recaldev.ga@gmail.com>
2026-02-03 20:31:41 -0300

    mejorando el PDF

--- components/pdf/SeccionFirmas.tsx
+++ components/pdf/SeccionFirmas.tsx
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'center',
         width: '40%',
+        marginTop: -15,
     },
     line: {
         borderTopWidth: 1,
