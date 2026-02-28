cec055417705fbb94e2cb991295d32d2a8b77eb8
GUILLERMO RECALDE <recaldev.ga@gmail.com>
2026-02-28 15:58:11 -0300

    Fix PDF font resolution error: add missing Roboto Italic

--- components/pdf/DenunciaPDF.tsx
+++ components/pdf/DenunciaPDF.tsx
@@ -13,6 +13,7 @@ Font.register({
     fonts: [
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf', fontWeight: 300 },
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf', fontWeight: 400 },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-italic-webfont.ttf', fontWeight: 400, fontStyle: 'italic' },
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-medium-webfont.ttf', fontWeight: 500 },
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf', fontWeight: 700 },
     ],
